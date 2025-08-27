3e3de1d789236852fd5a95195e46112e3687d476
JULIO CESAR CORREIA ALVES <julio.ccalves@senacsp.edu.br>
2025-08-27 15:57:12 -0300

    mysql-nodejs

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
+    //Conexão ao banco de dados uma vez no inicio
+    conexao.connect(function(error){
+    if (error) {
+        console.error("Erro ao conectar ao banco de dados:", error);
+        process.exit(); //encerrar o servidor caso a conexão falhe
+    }
+    });
+
 app.get('/', function(req, res){
     res.sendFile(__dirname+'/cadastro.html')
 });
@@ -19,32 +27,29 @@ app.post('/', function(req, res){
     var email = req.body.email;
     var senha = req.body.senha;
 
-    conexao.connect(function(error){
-    if(error) throw error;
-
-        //prevenindo SQL Injection
+    
+    //prevenindo SQL Injection
         var sql ="INSERT INTO estudante(nomecompleto, email, senha) VALUES (?, ?, ?)";
         conexao.query(sql, [nomecompleto, email, senha], function(error, result){
             if(error) throw error;
 
-            res.send("Estudante cadastrado com sucesso!" + result.insertId);
+            //res.send("Estudante cadastrado com sucesso!" + result.insertId);
+
+            res.redirect('/estudantes');
         });
         
     });
-});
+
 
 //Leitura do banco de dados
 app.get('/estudantes', function(req, res){
-    conexao.connect(function(error){
-    if(error) console.log(error);
-
 var sql = "select * from estudante";
 conexao.query(sql, function(error, result){
     if(error) console.log(error);
-    console.log(result);
+    //console.log(result);
+    res.render(__dirname+"/estudantes", {estudante:result});
         });
     });
-});
 
 app.listen(7000);
 
